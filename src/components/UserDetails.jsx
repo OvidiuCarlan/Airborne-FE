3bc9674309f3760cbc801c7c174288dc1d70daa0
Ovidiu Cârlan <481968@student.fontys.nl>
2023-11-22 18:58:19 +0100

    Profile page(design)

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,18 +1,34 @@
 import TokenManager from "../services/TokenManager";
+import styles from "./userDetails.module.css";
+import LogoPNG from '../assets/images/LogoPNG.png';
+import Avatar from '../assets/images/Avatar.png';
 
 function UserDetails(props) {
     const userDetails = props.userDetails.data;
     const claims = TokenManager.getClaims();
     const id = claims.userId;
 
-    console.log("UserDetails Component - UserDetails:", userDetails);
-    console.log("UserDetails Component - Claims:", claims);
-    console.log("UserDetails Component - User ID:", id);
-
     return (
-        <div className="user-details-container">
-            <p className="user-info">Username: <b>{userDetails.name}</b></p>
-            <p className="user-info">Email: <b>{userDetails.email}</b></p>
+        <div className={styles['user-details-container']}>
+
+            <div className={styles['user-details-top-container']}>
+
+                <div className={styles['avatar-container']}>
+                    <img src={Avatar} alt='Logo' className={styles['avatar']}/>
+                </div>
+
+                <div className={styles['logo-container']}>
+                    <img src={LogoPNG} alt='Logo' className={styles['logo']}/>  
+                </div>  
+
+            </div>
+
+
+            <div className={styles['user-details-bootom-container']}>
+                <p className={styles['user-info']}><b>{userDetails.name}</b></p>
+                <p className={styles['user-info']}><b>{userDetails.email}</b></p>
+            </div>
+            
         </div>
     );
 }
